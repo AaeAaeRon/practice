538fcbfdc59e5ceaf4b43f39b5a8e1a628740856
Aaron Briscoe <abriscoe2008@gmail.com>
2019-11-03 03:21:15 -0600

    practice amkes perfect

---
+++ javascript-prac/grocery.js
@@ -0,0 +1,10 @@
+var shopper = {
+    name: "Aaron",
+    age: 29,
+    isAlive: true,
+    greeting: function() {
+        return `Hi, my name is ${name} `
+    },
+    groceryCart: ["pizza", "Pop", "water"]
+};
+
